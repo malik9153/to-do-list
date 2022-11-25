641585fbf12a9e882691f4d0dbb06360dbbaaea4
malik016 <63663632+malik9153@users.noreply.github.com>
2022-11-25 14:57:08 +0000

    completeted task count

--- to-do-list-app/src/App.js
+++ to-do-list-app/src/App.js
@@ -2,9 +2,11 @@ import './App.css';
 import {useState} from 'react'
 
 function App() {
-  const [taskArr, setTaskArr] = useState(['Task 1', 'task 2', 'task 3'])
-  
+  const [taskArr, setTaskArr] = useState([])
+  const [completedTaskCount, setCompletedTaskCount] = useState(0);
   const [input, setInput] = useState('')
+
+
 
   const handleClick = () => {
     const id = taskArr.length + 1
@@ -13,12 +15,22 @@ function App() {
     )
     setInput('')
   }
-
-
-  const addTask = function() {
-    const newList =  [...taskArr, 'task 4']
-    return setTaskArr(newList)
-    }  
+  const handleComplete = (id) => {
+    let list = taskArr.map((task) => {
+      let item = {};
+      if(task.id == id){
+        if(!task.completed){
+          setCompletedTaskCount(completedTaskCount + 1);}
+          else { 
+            setCompletedTaskCount(completedTaskCount - 1);
+          }
+          item = { ...task, completed: !task.completed };
+        } else item = { ...task };
+        return item;
+      })
+      setTaskArr(list);
+    }
+ 
   
   return (
     <div className="App">
@@ -33,9 +45,17 @@ function App() {
             <h2>
               Not Started
             </h2>
+            {completedTaskCount}
             <ul>
             {taskArr.map(function (task) {
-              return (<li id={task.id} completed={task.completed}>{}<h2>{task}</h2>
+              return (<li id={task.id} completed={task.completed} 
+               onClick={() => handleComplete(task.id)}
+               style=
+              {{
+              listStyle: "none",
+              textDecoration: task.completed && "line-through",
+             }} >
+             {task.task}
               </li>)
             })}
             </ul>
